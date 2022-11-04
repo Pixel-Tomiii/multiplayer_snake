c9db62ba1c87be7102792cff3fe79eb6ae121a3e
BuildTools <55916455+Pixel-Tomiii@users.noreply.github.com>
2022-11-04 20:38:31 +0000

    update

--- application/game.py
+++ application/game.py
@@ -1,5 +1,8 @@
 import socket
 from socket import SOCK_DGRAM
+from multiprocessing import Process
+from multiprocessing import Queue
+from server.server import wait
 
 menu_items = [
     "play",
@@ -13,9 +16,27 @@ play_menu = [
 def on_play():
     pass
 
-def on_host(server_in, server_out):
+def start():
+    on_host()
+    pass
+
+def game_loop():
+    while True:
+        ui = input()
+
+        if ui == "host":
+            on_host()
+    
+
+def on_host():
     """Spawns a server to host a lobby
     Creates a subprocess to run the server."""
+    server_in = Queue()
+    server_out = Queue()
+    server_proc = Process(target=wait,
+                          name="snake_server",
+                          args=(server_in, server_out))
+    server_proc.start()
     print("Starting server")
     server_in.put("START")
     print("Started")
@@ -24,7 +45,6 @@ def on_host(server_in, server_out):
     sock = socket.socket(type=socket.SOCK_DGRAM).connect(address)
     print(address)
     
-        
 def load_options():
     pass
 
@@ -34,3 +54,4 @@ def save_options():
 def exit():
     pygame.exit()
     os._exit()
+
--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 
 
 if __name__ == "__main__":
-    from multiprocessing import Process
-    from multiprocessing import Queue
-    from server.server import wait
-    from application.game import on_host
+    from application.game import start
+    start()
 
-    server_in = Queue()
-    server_out = Queue()
-    server_proc = Process(target=wait,
-                          name="snake_server",
-                          args=(server_in, server_out))
-    server_proc.start()
-    print("Started process")
-
-    
-    on_host(server_in, server_out)
+else:
+    import server.server
 
 
